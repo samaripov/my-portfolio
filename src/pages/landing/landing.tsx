0534745d4c30e4b0815e615e4fb48e83d8b8b649
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-18 10:27:11 -0400

    Started work on the landing page.

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import landingStyles from "./landing.module.css";
+
 export default function LandingPage() {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div className={landingStyles.imageContainer}>
+      <div className={landingStyles.container}>
+        <div className={landingStyles.avatar}>
+          <img src="./pixels.jpg" alt="" />
+        </div>
+        <div className={landingStyles.whiteBox}>
+          <div className={landingStyles.title}>
+            <p></p>
+            <h1>Sirojiddin Aripov</h1>
+          </div>
+          <p>The Ohio State University Alumni</p>
+          <p>Bachelor's in Computer Science & Engineering</p>
+        </div>
+        
+        
+      </div>
     </div>
   );
 }
