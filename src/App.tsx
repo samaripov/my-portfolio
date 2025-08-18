e9c7e3d7ba53a0fdc93d0096b189a9adb5111edd
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-17 21:18:40 -0400

    Navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/navbar/navbar";
+import appStyle from "./App.module.css";
 
 export default function App() {
   return (
     <>
-      <Navbar />
+      <div className={appStyle.container}>
+        <Navbar />
+      </div>
       <Outlet />
     </>
   );
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,67 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
+import navbarStyle from "./navbar.module.css";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const [selectStyles, setSelectStyles] = useState({
+    background: "#1b7eef",
+  });
+
+  useEffect(() => {
+    switch (currentPath) {
+      case "/":
+        setSelectStyles({
+          background: "#1b7eef",
+        });
+        break;
+      case "/projects":
+        setSelectStyles({
+          background: "#eac435",
+          transform: "translate(9rem)",
+        });
+        break;
+      case "/blog":
+        setSelectStyles({
+          background: "#e40066",
+          transform: "translate(18rem)",
+        });
+        break;
+      case "/about_me":
+        setSelectStyles({
+          background: "#03cea4",
+          transform: "translate(27rem)",
+        });
+        break;
+    }
+  }, [currentPath]);
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/about_me">About me</Link>
-    </>
+    <div className={navbarStyle.container}>
+      <Link className={`${currentPath === "/" && navbarStyle.active} ${navbarStyle.button}`} to="/">
+        Home
+      </Link>
+      <Link
+        id="projects_link"
+        className={`${currentPath === "/projects" && navbarStyle.active} ${navbarStyle.button}`}
+        to="/projects"
+      >
+        Projects
+      </Link>
+      <Link className={`${currentPath === "/blog" && navbarStyle.active} ${navbarStyle.button}`} to="/blog">
+        Blog
+      </Link>
+      <Link
+        className={`${currentPath === "/about_me" && navbarStyle.active} ${navbarStyle.button}`}
+        to="/about_me"
+      >
+        About me
+      </Link>
+      <div
+        id="select"
+        style={selectStyles}
+        className={navbarStyle.select}
+      ></div>
+    </div>
   );
 }
