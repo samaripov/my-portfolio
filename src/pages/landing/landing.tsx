8b9e6204e4a1e4ab4023dcefef9ff0648521f900
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-18 12:59:33 -0400

    Updated the styling of the grid.

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -6,16 +6,18 @@ export default function LandingPage() {
   return (
     <div className={landingStyles.imageContainer}>
       <div className={landingStyles.container}>
-        <div className={landingStyles.avatar}>
-          <img src="./pixels.jpg" alt="" />
-        </div>
-        <div className={landingStyles.whiteBox}>
-          <div className={landingStyles.title}>
-            <p></p>
-            <h1>Sirojiddin Aripov</h1>
+        <div className={landingStyles.horizontalFlex}>
+          <div className={landingStyles.avatar}>
+            <img src="./pixels.jpg" alt="" />
           </div>
-          <p>The Ohio State University Alumni</p>
-          <p>Bachelor's in Computer Science & Engineering</p>
+          <div className={landingStyles.whiteBox}>
+            <div className={landingStyles.title}>
+              <p></p>
+              <h1>Sirojiddin Aripov</h1>
+            </div>
+            <p>The Ohio State University Alumni</p>
+            <p>Bachelor's in Computer Science & Engineering</p>
+          </div>
         </div>
         <div className={landingStyles.grid}>
           <ProjectCard
